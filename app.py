035676053d2eaa25609db7eb77e2d5826a28c7fa
kehindealawiye <128984208+kehindealawiye@users.noreply.github.com>
2025-05-12 21:44:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 
-creds_dict = st.secrets["gcp_service_account"]
-
 def get_gsheet_client():
     scope = [
         "https://spreadsheets.google.com/feeds",
