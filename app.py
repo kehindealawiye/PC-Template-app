24cac83864b4d53c174db8cfa2897257601565bf
kehindealawiye <128984208+kehindealawiye@users.noreply.github.com>
2025-05-15 11:01:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ custom_dropdowns = {
     "Percentage of Advance payment? (as specified in the award letter)": ["0%", "25%", "40%", "50%", "60%", "70%"],
     "Is there 5% retention?": ["0%", "5%"],
     "Vat": ["0%", "7.5%"],
-    "Percentage of Work done": ["Ongoing", "Completed"],
+    "Physical Stage of Work": ["Ongoing", "Completed"],
     "Address line 1": ["The Director,", "The Chairman,", "The Permanent Secretary,", "The General Manager,", "The Honourable Commissioner,", "The Special Adviser,"]
 }
 
